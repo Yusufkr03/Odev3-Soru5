48dfa37d337b0c976ed40f41a597f4b3770710ad
Yusufkr03 <yusufkr2903@gmail.com>
2024-11-07 16:35:02 +0300

    Add files via upload

---
+++ Odev3-Soru5/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random  rnd = new Random();
+int number = rnd.Next(1,20);
+Console.WriteLine("1 ile 20 arasında tutulan bir sayıyı tahmin edin.");
+int tahmin = int.Parse(Console.ReadLine());
+if (tahmin == number)
+{
+    Console.WriteLine("Tebrikler doğru tahmin");
+}
+else
+{
+    do
+    {
+        Console.WriteLine("Yanlış tahmin ettiniz. Tekrar bir sayı giriniz:");
+        tahmin = int.Parse(Console.ReadLine());
+
+
+    } while (tahmin != number);
+    Console.WriteLine("Tebrikler doğru tahmin");
+
+}
+Console.ReadKey();
